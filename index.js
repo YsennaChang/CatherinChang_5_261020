56512c043858f6e2e4f795776fac361ebc357732
Chang <changcatherinetchin@gmail.com>
2020-11-06 05:15:21 +0100

    Carousel dynamique fonctionnel

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-
+// Pour savoir comment le back renvoi les données//
+var request= new XMLHttpRequest();
+request.onreadystatechange = function (){
+    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        console.log(JSON.parse(this.responseText));
+    }
+}
+request.open ("GET", "http://localhost:3000/api/teddies");
+request.send();  
 // ==== I.Afficher les éléments de manière dynamique ====//
 
 // ==== I.1) Carousel dynamique ====//
+
 var imgURL = new XMLHttpRequest();
 imgURL.onreadystatechange = function (){
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        
-        
-        var carouselInner = document.getElementsByClassName("carousel-inner");
-        carouselInner.appendChild(carouselItem);
-        var carouselItem = document.getElementsByClassName("carousel-item");
-        carouselItem.appendChild(imageCarousel);
-
         var response = JSON.parse(this.responseText);
-        var imageCarousel = document.createElement("img");
-        
-        imageCarousel.src = response[0].imageUrl;
-        imageCarousel.classList.add("d-block","w-50","my-1","mx-auto","shadow-sm","rounded");
-        imageCarousel.alt = response[0].name;
-        console.log(imageCarousel);
+        var carouselInner = document.getElementsByClassName("carousel-inner")[0];
+        for (let i = 0; i < response.length; i++) {
+            var carouselItem = document.createElement("div");
+            carouselItem.classList.add("carousel-item");
+            if (i==0) {
+                carouselItem.classList.add("active");
+            }
+            carouselInner.appendChild(carouselItem);
+            var carouselItem = document.getElementsByClassName("carousel-item")[i];
+            var imageCarousel = document.createElement("img");
+            carouselItem.appendChild(imageCarousel);            
+            imageCarousel.src = response[i].imageUrl;
+            imageCarousel.classList.add("d-block","w-50","my-1","mx-auto","shadow-sm","rounded");
+            imageCarousel.alt = response[i].name;
+            // console.log(imageCarousel);
+        }
     }
 }
+
 imgURL.open ("GET", "http://localhost:3000/api/teddies");
 imgURL.send();  
 
