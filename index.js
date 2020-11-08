4573508dc810eeac0b3ffcf3873cfefeb61dc99b
Chang <changcatherinetchin@gmail.com>
2020-11-08 15:29:12 +0100

    factorisation des fonctions répétitives

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 //==== outil dev :  rendu du back ====//
 
 // var request= new XMLHttpRequest();
@@ -6,99 +7,104 @@
 //         console.log(JSON.parse(this.responseText));
 //     }
 // }
-// request.open ("GET", "http://localhost:3000/api/teddies");
+// request.open ("GET", routeAPI);
 // request.send();  
+
+var routeAPI = "http://localhost:3000/api/teddies";
+var response;
+function requestAPI () {
+    var myRequest = new XMLHttpRequest ();
+    myRequest.onreadystatechange = function(){
+        if (this.readyState == XMLHttpRequest.DONE && this.status ==200){
+            response = JSON.parse(this.responseText);
+            generateCarouselImages();
+            generateCardsInformations ();
+        }
+    }
+    myRequest.open("GET", routeAPI);
+    myRequest.send();
+}
+requestAPI();
+
 
 // ==== I.Afficher les éléments de manière dynamique ====//
 
 // ==== I.1) Carousel dynamique ====//
 
-var imgURL = new XMLHttpRequest();
-imgURL.onreadystatechange = function (){
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        var carouselInner = document.getElementsByClassName("carousel-inner")[0];
-        for (let i = 0; i < response.length; i++) {
-            var carouselItem = document.createElement("div");
-            carouselItem.classList.add("carousel-item");
-            if (i==0) {
-                carouselItem.classList.add("active");
-            }
-            carouselInner.appendChild(carouselItem);
-            var carouselItem = document.getElementsByClassName("carousel-item")[i];
-            var imageCarousel = document.createElement("img");
-            carouselItem.appendChild(imageCarousel);            
-            imageCarousel.src = response[i].imageUrl;
-            imageCarousel.classList.add("d-block","w-50","my-1","mx-auto","shadow-sm","rounded");
-            imageCarousel.alt = response[i].name;
+function generateCarouselImages (){
+    var carouselInner = document.getElementsByClassName("carousel-inner")[0];
+    for (let i = 0; i < response.length; i++) {
+        var carouselItem = document.createElement("div");
+        carouselItem.classList.add("carousel-item");
+        if (i==0) {
+            carouselItem.classList.add("active");
         }
+        carouselInner.appendChild(carouselItem);
+        var carouselItem = document.getElementsByClassName("carousel-item")[i];
+        var imageCarousel = document.createElement("img");
+        carouselItem.appendChild(imageCarousel);            
+        imageCarousel.src = response[i].imageUrl;
+        imageCarousel.classList.add("d-block","w-50","my-1","mx-auto","shadow-sm","rounded");
+        imageCarousel.alt = response[i].name;
     }
 }
-imgURL.open ("GET", "http://localhost:3000/api/teddies");
-imgURL.send();  
-
+    
 // ==== I.2) Card dynamique ====//
 
-var cardGenerator = new XMLHttpRequest();
-cardGenerator.onreadystatechange = function () {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        // pointage id teddiescard //
-        var teddiesCards = document.getElementById ("teddiesCard");
+function generateCardsInformations () {
+    // pointage id teddiescard //
+    var teddiesCards = document.getElementById ("teddiesCard");
 
-        // boucle de génération des infos des cards //
-        for (let i = 0; i < response.length; i++) {
-            //création block section card //
-            var card = document.createElement("section");
-            card.classList.add("col-12","col-md-6","col-lg-3","card");
-            teddiesCards.appendChild(card);
+    // boucle de génération des infos des cards //
+    for (let i = 0; i < response.length; i++) {
+    //création block section card //
+    var card = document.createElement("section");
+    card.classList.add("col-12","col-md-6","col-lg-3","card");
+    teddiesCards.appendChild(card);
 
-            // création block image//
-            var cardImage = document.createElement("img");
-            cardImage.classList.add("card-img-top", "shadow-sm");
-            cardImage.src = response[i].imageUrl;
-            cardImage.alt = response[i].name;
-            card.appendChild(cardImage);
+    // création block image//
+    var cardImage = document.createElement("img");
+    cardImage.classList.add("card-img-top", "shadow-sm");
+    cardImage.src = response[i].imageUrl;
+    cardImage.alt = response[i].name;
+    card.appendChild(cardImage);
 
-            // création block cardBody//
-            var cardBody = document.createElement("div");
-            cardBody.classList.add("card-body");
-            card.appendChild(cardBody);
+    // création block cardBody//
+    var cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    card.appendChild(cardBody);
 
-            //création block a//
+    //création block a//
 
-            var stretchedLink = document.createElement("a");
-            stretchedLink.classList.add("stretched-link");
-            var regex = / /g;
-            stretchedLink.href = response[i].name.replace(regex,"-")+".html";
-            cardBody.appendChild(stretchedLink);
+    var stretchedLink = document.createElement("a");
+    stretchedLink.classList.add("stretched-link");
+    var regex = / /g;
+    stretchedLink.href = response[i].name.replace(regex,"-")+".html";
+    cardBody.appendChild(stretchedLink);
 
-            //création block h3//
+    //création block h3//
 
-            var cardTitle = document.createElement("h3");
-            cardTitle.classList.add("card-title");
-            cardTitle.innerHTML = response[i].name;
-            stretchedLink.appendChild(cardTitle);
+    var cardTitle = document.createElement("h3");
+    cardTitle.classList.add("card-title");
+    cardTitle.innerHTML = response[i].name;
+    stretchedLink.appendChild(cardTitle);
 
-            //création bloc span//
+    //création bloc span//
 
-            var price = document.createElement("span");
-            price.classList.add("bg-primary","text-light","p-1");
-            var numberFormated = new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'}).format((response[i].price)/100)
-            price.innerHTML = numberFormated;
-            stretchedLink.appendChild(price);
+    var price = document.createElement("span");
+    price.classList.add("bg-primary","text-light","p-1");
+    var numberFormated = new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'}).format((response[i].price)/100)
+    price.innerHTML = numberFormated;
+    stretchedLink.appendChild(price);
 
-            //création block cardText//
-            var cardText = document.createElement("div");
-            cardText.classList.add("card-text");
-            cardText.innerHTML = response[i].description;
-            cardBody.appendChild(cardText);
-
-        }
+    //création block cardText//
+    var cardText = document.createElement("div");
+    cardText.classList.add("card-text");
+    cardText.innerHTML = response[i].description;
+    cardBody.appendChild(cardText);
     }
 }
-cardGenerator.open ("GET", "http://localhost:3000/api/teddies");
-cardGenerator.send(); 
+
 
 // ==== Envoyer des données au localStorage ====//
 
--- product.js
+++ product.js
@@ -1,3 +1,17 @@
+/** Infos à envoyer au backend
+ *
+ * Expects request to contain:
+ * contact: {
+ *   firstName: string,
+ *   lastName: string,
+ *   address: string,
+ *   city: string,
+ *   email: string
+ * }
+ * products: [string] <-- array of product _id
+ *
+ */
+
 //==== outil dev :  rendu du back ====//
 
 // var request= new XMLHttpRequest();
@@ -10,98 +24,76 @@
 // request.send();  
 
 // ==== II. Afficher les éléments page produit en dynamique ====//
-// Récupérer le nom du Teddy selectionné //
-var urlcourante = document.location.href; 
- 
-// Supprimons l'éventuel dernier slash de l'URL et le .html//
-urlcourante = urlcourante.replace(/.html$|\/$/, "");
-// remplacer les "-" par des " " pour correspondre au formatage dans le backend //
-urlcourante = urlcourante.replace(/-/g, " ");
-// Gardons dans la variable queue_url uniquement la portion derrière le dernier slash de urlcourante
-var queue_url = urlcourante.substring (urlcourante.lastIndexOf( "/" )+1 );
 
-// console.log(queue_url);
 
-var request = new XMLHttpRequest();
-request.onreadystatechange = function (){
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        
-        var response = JSON.parse(this.responseText);
+var cart = [];
 
-        for (let i=0; i< response.length; i++){
 
-            if (response[i].name===queue_url){
-                // console.log(i);
-                // Remplacer la photo //
-                var photoTeddy = document.getElementById("photoTeddy");
-                photoTeddy.src = response[i].imageUrl;
-                photoTeddy.alt = response[i].name;
-                
-                // Remplacer le nom du Teddy titre et h2//
-                var nomTeddy = document.getElementById("nameTeddy");
-                var title = document.getElementsByTagName("title")[0];
-                nomTeddy.innerHTML = response[i].name;
-                title.innerHTML = response[i].name;
-
-                // Remplacer le prix du Teddy //
-                var priceTeddy = document.getElementById("priceTeddy");
-                var numberFormated = new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'}).format((response[i].price)/100)
-                priceTeddy.innerHTML = numberFormated;
-
-                // Incrémente les options //
-
-                var colorSection = document.getElementById("colorOption");
-                for (let j=0; j< response[i].colors.length; j++){
-                    colorOption = document.createElement("option");
-                    colorOption.innerHTML = response[i].colors[j];
-                    colorSection.appendChild(colorOption);
-                };
-
-                // Remplacer la description du Teddy //
-                var descriptionTeddy = document.getElementById("descriptionTeddy");
-                descriptionTeddy.innerHTML = response[i].description;
-            }
+var routeAPI = "http://localhost:3000/api/teddies";
+var response;
+function requestAPI () {
+    var myRequest = new XMLHttpRequest ();
+    myRequest.onreadystatechange = function(){
+        if (this.readyState == XMLHttpRequest.DONE && this.status ==200){
+            response = JSON.parse(this.responseText);
+            replaceStaticByDynamicInformations ();
+            // ajoute les EventListeners "click" sur les boutons "ajouter au panier", ajoute les products_id dans le panier, met le panier dans le localStorage
+            addProductInCart ();
         }
+    }
+    myRequest.open("GET", routeAPI);
+    myRequest.send();
+}
+requestAPI();
+
+
+function replaceStaticByDynamicInformations () {
+    for (let i=0; i< response.length; i++){
+        var photoTeddy = document.getElementsByClassName("photoTeddy")[i];
+        photoTeddy.src = response[i].imageUrl;
+        photoTeddy.alt = response[i].name;
+        
+        // Remplacer le nom du Teddy titre et h2//
+        var nomTeddy = document.getElementsByClassName("nameTeddy")[i];
+        // var title = document.getElementsByTagName("title")[0];
+        nomTeddy.innerHTML = response[i].name;
+        // title.innerHTML = response[i].name;
+
+        // Remplacer le prix du Teddy //
+        var priceTeddy = document.getElementsByClassName("priceTeddy")[i];
+        var numberFormated = new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'}).format((response[i].price)/100)
+        priceTeddy.innerHTML = numberFormated;
+
+        // Incrémente les options //
+
+        var colorSection = document.getElementsByClassName("colorOption")[i];
+        for (let j=0; j< response[i].colors.length; j++){
+            colorOption = document.createElement("option");
+            colorOption.innerHTML = response[i].colors[j];
+            colorSection.appendChild(colorOption);
+        };
+
+        // Remplacer la description du Teddy //
+        var descriptionTeddy = document.getElementsByClassName("descriptionTeddy")[i];
+        descriptionTeddy.innerHTML = response[i].description;
     }
 }
-request.open ("GET", "http://localhost:3000/api/teddies");
-request.send();
 
 //===== Ajouter article au localStorage ==== //
-var panier = [];
-var ajouterPanier = document.getElementById("ajouterPanier");
-ajouterPanier.addEventListener("click", (event) =>{
-    event.preventDefault();
-    var addProduct = new XMLHttpRequest();
-    addProduct.onreadystatechange = function(){
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            var response = JSON.parse(this.responseText);
-            for (let i=0; i< response.length; i++){
-                if (response[i].name===queue_url){
-                    var productId = response[i]._id;
-                    panier.push(productId);
-
-                }
-            }
-        }
+function addProductInCart () {
+    for (let i=0; i< response.length; i++){
+        var btnAddToCart = document.getElementsByClassName("btn-add-to-cart")[i];
+        btnAddToCart.addEventListener("click", (event) =>{
+            event.preventDefault();
+            var productId = response[i]._id;
+            cart.push(productId);
+            addCartInLocalStorage ();
+        });
     }
-    addProduct.open("GET", "http://localhost:3000/api/teddies");
-    addProduct.send(); 
-    localStorage.setItem("products",JSON.stringify(panier));
-    console.log(panier);
-})
+}
 
+function addCartInLocalStorage () {
+    localStorage.setItem("products",JSON.stringify(cart));
+    console.log(cart);
 
-    /**
-     *
-     * Expects request to contain:
-     * contact: {
-     *   firstName: string,
-     *   lastName: string,
-     *   address: string,
-     *   city: string,
-     *   email: string
-     * }
-     * products: [string] <-- array of product _id
-     *
-     */
+}
